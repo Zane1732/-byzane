8484e461e1d4f6f02290296f1bebcb77d1ba6b0d
animelegendpro <167015173+animelegendpro@users.noreply.github.com>
2025-01-03 12:33:33 +0530

    Update _config.php

--- _config.php
+++ _config.php
@@ -7,7 +7,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$websiteTitle = "AniPaca";
+$websiteTitle = "AniMatrix";
 $websiteUrl = "//{$_SERVER['SERVER_NAME']}";
 $websiteLogo = $websiteUrl . "/public/logo/logo.png";
 $contactEmail = "@gmail.com";
@@ -20,9 +20,9 @@ $twitter = "https://x.com/PacaHat";
 
 // all the api you need
 
-$api = "your-hosted-api.com/api/v2/hianime"; //https://github.com/ghoshRitesh12/aniwatch-api Also use this in src/component/qtip.php
-$zpi = "your-hosted-api.com/api"; //https://github.com/itzzzme/anime-api
-$proxy = "your-hosted-proxy.com/cors?url="; //https://github.com/shashstormer/m3u8_proxy-cors
+$api = "https://aniwatch-api-iota-ten.vercel.app"; //https://github.com/ghoshRitesh12/aniwatch-api Also use this in src/component/qtip.php
+$zpi = "https://zenime-api-sand.vercel.app"; //https://github.com/itzzzme/anime-api
+$proxy = "m3u8-proxy-cors-iota-lac.vercel.app/cors?url="; //https://github.com/shashstormer/m3u8_proxy-cors
 
 
 $banner = $websiteUrl . "/public/images/banner.png";
